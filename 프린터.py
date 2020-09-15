21246ef7d8052d1d1281fa540c28301b6d8d6422
Ella.Ha <63627272+MinjiHa@users.noreply.github.com>
2020-09-15 15:50:38 +0900

    Programmers Level2

--- 프린터.py
+++ 프린터.py
@@ -30,3 +30,19 @@ def solution(priorities, location):
         if location==printer[i][1]:
             answer=i+1
     return answer
+
+"""
+any()를 쓰면 더 간단하게 문제를 풀 수 있다.
+"""
+
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
